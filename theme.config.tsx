e41c3e25a480078a5a94f9182c923afbbe168879
i207M <connect@mail.i207m.top>
2024-08-12 14:52:26 +0800

    update front page

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>PINNacle</span>,
+  logo: <span><big><b>PINN</b></big>acle</span>,
   project: {
     link: 'https://github.com/i207M/PINNacle',
   },
